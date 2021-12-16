b96df991b0e5c5bcafe2d88fbc66381f5b9f03cd
Klay Clarke <klayaclarke@gmail.com>
2021-12-16 17:41:37 -0500

    new route '/add' for adding cafes to db - allows user to enter new cafe's parameters (key-value pairs) via route

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+import os
 import random
-from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, render_template, request
+
 
 app = Flask(__name__)
+app.secret_key = os.environ.get('FLASK_SECRET_KEY')
 
 # Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
@@ -28,12 +31,29 @@ class Cafe(db.Model):
         return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
 
+db.create_all()
 all_cafes = db.session.query(Cafe).all()
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route('/add', methods=['POST'])
+def add_cafe():
+    new_cafe = Cafe(name=request.form.get('name'), map_url=request.form.get('map_url'),
+                    img_url=request.form.get('img_url'),
+                    location=request.form.get('location'), seats=request.form.get('seats'),
+                    has_toilet=bool(request.form.get('has_toilet')),
+                    has_wifi=bool(request.form.get('has_wifi')),
+                    has_sockets=bool(request.form.get('has_sockets')),
+                    can_take_calls=bool(request.form.get('can_take_calls')),
+                    coffee_price=request.form.get('coffee_price'))
+    db.session.add(new_cafe)
+    db.session.commit()
+    success_json = {'response': {'success': 'Successfully added the new cafe.'}}
+    return jsonify(success_json)
 
 
 @app.route('/search', methods=['GET'])
@@ -58,15 +78,6 @@ def get_all_cafes():
 def get_random_cafe():
     random_cafe = random.choice(all_cafes)
     return jsonify(cafe=random_cafe.to_dict())
-
-
-# HTTP GET - Read Record
-
-# HTTP POST - Create Record
-
-# HTTP PUT/PATCH - Update Record
-
-# HTTP DELETE - Delete Record
 
 
 if __name__ == '__main__':
